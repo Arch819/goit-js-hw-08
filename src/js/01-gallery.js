a4b97f4577f30502f4cd42814f8ad89c9d1b574c
Viacheslav <gabon.qwa@gmail.com>
2023-06-28 11:07:03 +0300

    task 01 - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,27 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+
+const murkup = galleryItems
+  .map(({preview, original, description}) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img 
+          class="gallery__image" 
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+      </li>`
+  })
+  .join('');
+
+galleryRef.innerHTML = murkup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
