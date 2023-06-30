ea6527da6f4e46ed16ca67a188007e462acf3001
Viacheslav <gabon.qwa@gmail.com>
2023-06-30 10:43:44 +0300

    task  03 - fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,16 @@
 import throttle from 'lodash.throttle';
 
 const formRef = document.querySelector('.feedback-form');
+const inputRef = formRef.querySelector('input');
+const textareaRef = formRef.querySelector('textarea');
 
 const USER_STORAGE = 'feedback-form-state';
 availableMessage();
 
 formRef.addEventListener('submit', onFormSubmit);
-formRef.addEventListener('input', onUserDate);
+// formRef.addEventListener('input', onUserDate);
+inputRef.addEventListener('input', throttle(onUserDate, 500));
+textareaRef.addEventListener('input', throttle(onUserDate, 500));
 
 function onFormSubmit(event) {
   event.preventDefault();
@@ -17,11 +21,23 @@ function onFormSubmit(event) {
   localStorage.removeItem(USER_STORAGE);
 }
 
-function onUserDate(event) {
-  const { email, message } = event.currentTarget.elements;
+// function onUserDate(event) {
+//   const { email, message } = event.currentTarget.elements;
 
-  const emailUser = email.value;
-  const messageUser = message.value;
+//   const emailUser = email.value;
+//   const messageUser = message.value;
+
+//   const userDate = {
+//     email: emailUser,
+//     message: messageUser,
+//   };
+  
+//     localStorage.setItem(USER_STORAGE, JSON.stringify(userDate));
+// }
+
+function onUserDate() {
+  const emailUser = inputRef.value;
+  const messageUser = textareaRef.value;
 
   const userDate = {
     email: emailUser,
